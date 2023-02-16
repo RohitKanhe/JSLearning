de90ae922a7c680f74ab1173d01aaf28c46bb8ef
RohitKanhe <rohitkanhe76@gmail.com>
2023-02-16 15:19:03 +0530

    self introduction using js
    self introduction using js

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+console.log("*************** Step 1 *******************");
+    var myfullName;
+    console.log(myfullName);
+    myfullName = " Rohit Kanhe";
+    console.log("My Full name is", myfullName);
+    myfullName = " Rohit Vilasrao Kanhe";
+    console.log("My First Name, Middle Name and Last Name is", myfullName);
+
+
+    console.log("************** Step 2 *****************");
+    var myLove = "I Love only JS";
+    console.log (myLove);
+
+
+    console.log("***************** Step 3 ****************");
+    var firstName = "Rohit";
+    var LastName = "Kanhe";
+    var age = "25";
+    var address = "Amravati"
+    var pincode = "444803";
+    var gender = "Male";
+    var isMarried = "No Waiting";
+    
+
+    console.log("My Name is:" , firstName, "My Surname is:" , LastName, "my Age is:" , age, 
+    "My PinCode is:" , pincode, "My address is:" , address, "Married:", isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,22 @@
 console.log(" Rohit Kanhe ");
+console.log("25");
+console.log("pune");
+
+var fullName = "Rohit Kanhe";
+console.log(fullName);
+console.log(fullName);
+
+var city; //Variable Declration
+console.log(city);
+city="pune"; //Variable initializtion
+console.log(city);
+city="surat"; //Variable update
+console.log(city);
+
+var age="25"; //Variable Declration and initializtion in same line
+age="26";
+age="27";
+console.log(age);
+
+var universityName = "SGBAU"; //Variable initializtion
+console.log(universityName);
